eda35d410a68ec3cd4107c6911a7121f09da104c
A-lex-G <alexey803@gmail.com>
2023-03-21 17:17:15 +0200

    2 of 10 tasks

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,24 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+const elements = ingredients.map((ingredient) => {
+  const elementOfList = document.createElement("li");
+
+  elementOfList.textContent = ingredient;
+
+  elementOfList.classList.add("item");
+
+  return elementOfList;
+});
+
+list.append(...elements);
+
+console.log(list);
