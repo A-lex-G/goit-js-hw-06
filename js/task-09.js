98925b2dd371b3787b05c6db5bbdbf7a777a0f23
A-lex-G <alexey803@gmail.com>
2023-03-24 13:44:13 +0200

    task 9 of 10

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const buttonEl = document.querySelector(".change-color");
+const colorName = document.querySelector(".color");
+const backGround = document.body;
+
+buttonEl.addEventListener("click", onButtonClick);
+
+function onButtonClick() {
+  colorName.textContent = getRandomHexColor();
+  backGround.style.backgroundColor = colorName.textContent;
+}
