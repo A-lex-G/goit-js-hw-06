de40ef7db78c64701ace65caa9fa6b8e1b1b0e77
A-lex-G <alexey803@gmail.com>
2023-03-24 12:21:02 +0200

    task 8 of 10

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+
+  if (formElements.email.value === "" || formElements.password.value === "") {
+    alert("All form iputs should be filled");
+  } else {
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+    const dataObject = {
+      email,
+      password,
+    };
+    console.log(`Email: ${email}. Password: ${password}.`);
+    console.log(dataObject);
+  }
+  event.currentTarget.reset();
+}
