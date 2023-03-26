6d1c4079c906d5c79f71fb0724e316954393dd67
A-lex-G <alexey803@gmail.com>
2023-03-26 15:24:23 +0300

    task 10 of 10

--- js/task-10.js
+++ js/task-10.js
@@ -8,42 +8,25 @@ const container = document.querySelector("#controls");
 let inputEl = container.firstElementChild;
 const buttons = document.querySelectorAll("#controls button");
 const targetContainer = document.querySelector("#boxes");
-const createButton = buttons[0];
-const destroyButton = buttons[1];
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
 
 createButton.addEventListener("click", createBoxes);
 
 function createBoxes(amount) {
+  amount = inputEl.value;
+  let size = 30;
   for (let amount = 0; amount < Number(inputEl.value); amount += 1) {
-    targetContainer.append(document.createElement("div"));
-    targetContainer.firstElementChild.style.width = "30px";
-    targetContainer.firstElementChild.style.height = "30px";
-    targetContainer.firstElementChild.style.backgroundColor = "teal";
-    console.log(targetContainer.firstElementChild.style.width);
-
-    for (let item of targetContainer.children) {
-      item.style.width +=
-        targetContainer.previousElementSibling.style.width + 10 + "px";
-      item.style.height +=
-        targetContainer.previousElementSibling.style.height + 10 + "px";
-      item.style.backgroundColor = "teal";
-    }
-
-    // targetContainer.nextElementSibling.style.width +=
-    //   targetContainer.previousElementSibling.style.width + 10 + "px";
-    // targetContainer.nextElementSibling.style.height +=
-    //   Number.parseInt(targetContainer.previousElementSibling.style.height) +
-    //   10 +
-    //   "px";
-    // targetContainer.nextElementSibling.style.backgroundColor = "teal";
-    // console.log(Number.parseInt(targetContainer.lastElementChild.style.width));
-    // console.dir(targetContainer);
+    const innerContainer = document.createElement("div");
+    size += 10;
+    innerContainer.style.width = size + "px";
+    innerContainer.style.height = size + "px";
+    innerContainer.style.backgroundColor = getRandomHexColor();
+    targetContainer.append(innerContainer);
   }
 }
-console.dir(inputEl);
-console.log(inputEl.value);
-console.log(createButton);
-console.log(destroyButton);
-console.log(buttons);
-console.log(buttons[0].dataset);
-console.log(buttons[1].dataset);
+destroyButton.addEventListener("click", destroyBoxes);
+
+function destroyBoxes() {
+  targetContainer.innerHTML = "";
+}
